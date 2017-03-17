10966c4269ee0405827c0860c90427be6d3b38a3
dani maietta <dani maietta>
2017-03-16 20:17:55 -1200

    background-image - gulpsass

--- Planning/gulpfile.js
+++ Planning/gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 //Synchronize the browser with the app folder
@@ -10,8 +11,18 @@ gulp.task('browserSync', function(){
    })
 });
 
+gulp.task('sass', function(){
+	  return gulp.src('WebContent/scss/*.scss')
+	    .pipe(sass())
+	    .pipe(gulp.dest('WebContent/css'))
+	    .pipe(browserSync.reload({
+	      stream: true
+	  }))
+});
+
 // Actualize the scss, html and js files
-gulp.task('watch', ['browserSync'], function(){
+gulp.task('watch', ['sass','browserSync'], function(){
+   gulp.watch('WebContent/scss/*.scss', ['sass']);
    gulp.watch('WebContent/*.html', browserSync.reload);
    //gulp.watch('WebContent/js/*.js');
 });
