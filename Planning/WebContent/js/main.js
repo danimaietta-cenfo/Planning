7ac0cdb989637dd5efa0ce7b7941cd10870c1201
dani maietta <dani maietta>
2017-03-18 22:49:08 -1200

    background changing, buttons, bootstrap added, materialize deleted

--- Planning/WebContent/js/main.js
+++ Planning/WebContent/js/main.js
@@ -1,3 +1,30 @@
- function validate(){
+function validate(){
 	var name = StringUtil.method();
 }
+ 
+  var imgs = ['images/office1.jpg',
+            'images/office2.jpg',
+            'images/office3.jpg',
+            'images/office4.jpg',
+            'images/office5.jpg'
+            ];
+  var i = 1; 
+  document.getElementById("bgimg").style.backgroundImage = "url("+imgs[i]+")";
+  setTimeout(changeImage, 10000);
+
+ function changeImage(){
+	i++;
+	if(i === 4){
+		i = 0;
+	}
+	$("#bgimg").fadeOut(200, function(){
+		document.getElementById("bgimg").style.backgroundImage = "url("+imgs[i]+")";
+		$("#bgimg").fadeIn(200);
+	});
+	setTimeout(changeImage, 10000);
+ }
+
+ 
+//document.body.style.backgroundImage = "url("+img[i]+")";
+
+ 
